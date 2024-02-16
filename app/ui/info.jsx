ec2090e221663a641b6c4716c561703b70210c72
tev <tevelshorz@gmail.com>
2024-02-16 14:29:25 +0800

    feat; added copy right

--- app/ui/info.jsx
+++ app/ui/info.jsx
@@ -3,8 +3,11 @@ import styles from "./info.module.css";
 
 export default function Info() {
   return (
-    <a className={styles.info} href="https://github.com/trevelling" target="_blank" rel="noopener noreferrer" >
-      made w 💕 by tev | last updated: 16/2/2024
-    </a>
+    <footer>
+      <a className={styles.info} href="https://github.com/trevelling" target="_blank" rel="noopener noreferrer" >
+      &copy; {new Date().getFullYear()} made w 💕 by tev 
+      </a>
+    </footer>
+  
   );
 }
